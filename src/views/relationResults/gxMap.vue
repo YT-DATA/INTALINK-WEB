f1529e5047a56695cf77d08d243b24bb61d0fd52
dzr0708@163.com <your-email@example.com>
2025-01-08 17:03:15 +0800

    成果图修改

--- src/views/relationResults/gxMap.vue
+++ src/views/relationResults/gxMap.vue
@@ -19,14 +19,67 @@
         <!--            </div>-->
         <!--          </div>-->
         <!--        </template>-->
+        <template #graph-plug>
+          <div style="position:absolute;z-index:700;left:20px;top:20px;height:50px;padding-top:6px;padding-left: 30px;padding-right:30px;border: #efefef solid 1px;color: #555555;border-radius: 10px;background-color: rgba(255,255,255,0.79);font-size: 12px;">
+            <div style="">
+              <el-form :model="queryParams" ref="queryRef" :inline="true">
+                <el-form-item :label="$t('columnManagement.Belonging_model')" prop="dataModelId">
+                  <el-select
+                      v-model="queryParams.dataModelId"
+                      :placeholder="$t('columnManagement.Belonging_model_Tip')"
+                      :teleported="false"
+                      @change="handleModelValueChange"
+                      clearable
+                  >
+                    <el-option
+                        v-for="item in optionsModel"
+                        :key="item.dataModelId"
+                        :label="item.dataModelName"
+                        :value="item.dataModelId"
+                    />
+                  </el-select>
+                </el-form-item>
+                <el-form-item :label="$t('columnManagement.Data_table')" prop="dataTableId">
+                  <el-select
+                      v-model="queryParams.dataTableId"
+                      :placeholder="$t('columnManagement.Data_table_Tip')"
+                      @change="handleTableValueChange"
+                      clearable
+                      :teleported="false"
+                  >
+                    <el-option
+                        v-for="item in optionsTable"
+                        :key="item.dataTableId"
+                        :label="item.dataTableName"
+                        :value="item.dataTableId"
+                    />
+                  </el-select>
+                </el-form-item>
+                <el-form-item :label="$t('columnManagement.Data_Item_Name')" prop="dataColumnName">
+                  <el-input
+                      v-model="queryParams.dataColumnName"
+                      :placeholder="$t('columnManagement.Data_Item_Name_Tip')"
+                      clearable
+                      style="width: 200px"
+                      @keyup.enter="handleQuery"
+                  />
+                </el-form-item>
+              </el-form>
+
+            </div>
+          </div>
+        </template>
       </RelationGraph>
     </div>
   </div>
 </template>
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue';
+import {ref, onMounted, onBeforeUnmount, reactive} from 'vue';
 import RelationGraph from 'relation-graph/vue3';
-
+import {
+  dataModelSelectAll,
+  selectAllByModelId
+} from "@/api/configuration/configuration.js";
 // 使用 ref 来声明 graphRef，初始值设为 null，用于后续获取组件实例引用
 const graphRef = ref(null);
 // 响应式数据声明
@@ -37,7 +90,12 @@ const graphOptions = ref({
   allowSwitchLineShape: true,
   allowSwitchJunctionPoint: true,
   defaultLineShape: 1,
-  backgroundColor: 'transpanret',
+  backgroundColor: '#00001a', // 深蓝色，类似星空背景
+  // defaultLineColor: '#9999FF', // 淡紫色，与星空背景搭配协调
+  defaultNodeColor: '#87CEFA',
+  // defaultNodeColor: 'rgba(238, 178, 94, 1)',
+  // 连线颜色配置，采用淡蓝色，与节点颜色相呼应，呈现科技感
+  defaultLineColor: '#7a99af',
   layouts: [
     {
       label: '自动布局',
@@ -47,10 +105,17 @@ const graphOptions = ref({
   ],
   defaultJunctionPoint: 'border',
 });
-
+const queryParams = reactive({
+  dataColumnName: undefined,
+  dataModelId: undefined,
+  dataTableId: undefined,
+});
+const optionsModel = ref([]) //模型下拉选
+const optionsTable = ref([]) //表下拉选
 // 生命周期钩子
 onMounted(() => {
   showGraph();
+  ModelSelectAll();
 });
 onBeforeUnmount(() => {
   console.log('beforeDestroy stop layout');
@@ -60,232 +125,66 @@ onBeforeUnmount(() => {
 // 展示图谱的方法
 const showGraph = () => {
   const __graph_json_data = {
-    'rootId': 'a',
     'nodes': [
       {'id': 'a', 'text': 'a'},
       {'id': 'b', 'text': 'b'},
       {'id': 'b1', 'text': 'b1'},
       {'id': 'b1-1', 'text': 'b1-1'},
-      {'id': 'b1-2', 'text': 'b1-2'},
-      {'id': 'b1-3', 'text': 'b1-3'},
-      {'id': 'b1-4', 'text': 'b1-4'},
-      {'id': 'b1-5', 'text': 'b1-5'},
-      {'id': 'b1-6', 'text': 'b1-6'},
       {'id': 'b2', 'text': 'b2'},
       {'id': 'b2-1', 'text': 'b2-1'},
-      {'id': 'b2-2', 'text': 'b2-2'},
-      {'id': 'b2-3', 'text': 'b2-3'},
-      {'id': 'b2-4', 'text': 'b2-4'},
       {'id': 'b3', 'text': 'b3'},
       {'id': 'b3-1', 'text': 'b3-1'},
-      {'id': 'b3-2', 'text': 'b3-2'},
-      {'id': 'b3-3', 'text': 'b3-3'},
-      {'id': 'b3-4', 'text': 'b3-4'},
-      {'id': 'b3-5', 'text': 'b3-5'},
-      {'id': 'b3-6', 'text': 'b3-6'},
-      {'id': 'b3-7', 'text': 'b3-7'},
       {'id': 'b4', 'text': 'b4'},
       {'id': 'b4-1', 'text': 'b4-1'},
       {'id': 'b4-2', 'text': 'b4-2'},
       {'id': 'b4-3', 'text': 'b4-3'},
-      {'id': 'b4-4', 'text': 'b4-4'},
-      {'id': 'b4-5', 'text': 'b4-5'},
-      {'id': 'b4-6', 'text': 'b4-6'},
-      {'id': 'b4-7', 'text': 'b4-7'},
-      {'id': 'b4-8', 'text': 'b4-8'},
-      {'id': 'b4-9', 'text': 'b4-9'},
       {'id': 'b5', 'text': 'b5'},
       {'id': 'b5-1', 'text': 'b5-1'},
-      {'id': 'b5-2', 'text': 'b5-2'},
-      {'id': 'b5-3', 'text': 'b5-3'},
-      {'id': 'b5-4', 'text': 'b5-4'},
       {'id': 'b6', 'text': 'b6'},
       {'id': 'b6-1', 'text': 'b6-1'},
-      {'id': 'b6-2', 'text': 'b6-2'},
-      {'id': 'b6-3', 'text': 'b6-3'},
-      {'id': 'b6-4', 'text': 'b6-4'},
-      {'id': 'b6-5', 'text': 'b6-5'},
-      {'id': 'c', 'text': 'c'},
-      {'id': 'c1', 'text': 'c1'},
-      {'id': 'c1-1', 'text': 'c1-1'},
-      {'id': 'c1-2', 'text': 'c1-2'},
-      {'id': 'c1-3', 'text': 'c1-3'},
-      {'id': 'c1-4', 'text': 'c1-4'},
-      {'id': 'c1-5', 'text': 'c1-5'},
-      {'id': 'c1-6', 'text': 'c1-6'},
-      {'id': 'c1-7', 'text': 'c1-7'},
-      {'id': 'c2', 'text': 'c2'},
-      {'id': 'c2-1', 'text': 'c2-1'},
-      {'id': 'c2-2', 'text': 'c2-2'},
-      {'id': 'c3', 'text': 'c3'},
-      {'id': 'c3-1', 'text': 'c3-1'},
-      {'id': 'c3-2', 'text': 'c3-2'},
-      {'id': 'c3-3', 'text': 'c3-3'},
-      {'id': 'd', 'text': 'd'},
-      {'id': 'd1', 'text': 'd1'},
-      {'id': 'd1-1', 'text': 'd1-1'},
-      {'id': 'd1-2', 'text': 'd1-2'},
-      {'id': 'd1-3', 'text': 'd1-3'},
-      {'id': 'd1-4', 'text': 'd1-4'},
-      {'id': 'd1-5', 'text': 'd1-5'},
-      {'id': 'd1-6', 'text': 'd1-6'},
-      {'id': 'd1-7', 'text': 'd1-7'},
-      {'id': 'd1-8', 'text': 'd1-8'},
-      {'id': 'd2', 'text': 'd2'},
-      {'id': 'd2-1', 'text': 'd2-1'},
-      {'id': 'd2-2', 'text': 'd2-2'},
-      {'id': 'd3', 'text': 'd3'},
-      {'id': 'd3-1', 'text': 'd3-1'},
-      {'id': 'd3-2', 'text': 'd3-2'},
-      {'id': 'd3-3', 'text': 'd3-3'},
-      {'id': 'd3-4', 'text': 'd3-4'},
-      {'id': 'd3-5', 'text': 'd3-5'},
-      {'id': 'd4', 'text': 'd4'},
-      {'id': 'd4-1', 'text': 'd4-1'},
-      {'id': 'd4-2', 'text': 'd4-2'},
-      {'id': 'd4-3', 'text': 'd4-3'},
-      {'id': 'd4-4', 'text': 'd4-4'},
-      {'id': 'd4-5', 'text': 'd4-5'},
-      {'id': 'd4-6', 'text': 'd4-6'},
       {'id': 'e', 'text': 'e'},
       {'id': 'e1', 'text': 'e1'},
       {'id': 'e1-1', 'text': 'e1-1'},
-      {'id': 'e1-2', 'text': 'e1-2'},
-      {'id': 'e1-3', 'text': 'e1-3'},
-      {'id': 'e1-4', 'text': 'e1-4'},
-      {'id': 'e1-5', 'text': 'e1-5'},
-      {'id': 'e1-6', 'text': 'e1-6'},
-      {'id': 'e2', 'text': 'e2'},
-      {'id': 'e2-1', 'text': 'e2-1'},
-      {'id': 'e2-2', 'text': 'e2-2'},
-      {'id': 'e2-3', 'text': 'e2-3'},
-      {'id': 'e2-4', 'text': 'e2-4'},
-      {'id': 'e2-5', 'text': 'e2-5'},
-      {'id': 'e2-6', 'text': 'e2-6'},
-      {'id': 'e2-7', 'text': 'e2-7'},
-      {'id': 'e2-8', 'text': 'e2-8'},
-      {'id': 'e2-9', 'text': 'e2-9'}
     ],
     'lines': [
       {'from': 'a', 'to': 'b'},
       {'from': 'b', 'to': 'b1'},
       {'from': 'b1', 'to': 'b1-1'},
-      {'from': 'b1', 'to': 'b1-2'},
-      {'from': 'b1', 'to': 'b1-3'},
-      {'from': 'b1', 'to': 'b1-4'},
-      {'from': 'b1', 'to': 'b1-5'},
-      {'from': 'b1', 'to': 'b1-6'},
       {'from': 'b', 'to': 'b2'},
-      {'from': 'b2', 'to': 'b2-1'},
-      {'from': 'b2', 'to': 'b2-2'},
-      {'from': 'b2', 'to': 'b2-3'},
-      {'from': 'b2', 'to': 'b2-4'},
       {'from': 'b', 'to': 'b3'},
       {'from': 'b3', 'to': 'b3-1'},
-      {'from': 'b3', 'to': 'b3-2'},
-      {'from': 'b3', 'to': 'b3-3'},
-      {'from': 'b3', 'to': 'b3-4'},
-      {'from': 'b3', 'to': 'b3-5'},
-      {'from': 'b3', 'to': 'b3-6'},
-      {'from': 'b3', 'to': 'b3-7'},
       {'from': 'b', 'to': 'b4'},
       {'from': 'b4', 'to': 'b4-1'},
       {'from': 'b4', 'to': 'b4-2'},
       {'from': 'b4', 'to': 'b4-3'},
-      {'from': 'b4', 'to': 'b4-4'},
-      {'from': 'b4', 'to': 'b4-5'},
-      {'from': 'b4', 'to': 'b4-6'},
-      {'from': 'b4', 'to': 'b4-7'},
-      {'from': 'b4', 'to': 'b4-8'},
-      {'from': 'b4', 'to': 'b4-9'},
       {'from': 'b', 'to': 'b5'},
       {'from': 'b5', 'to': 'b5-1'},
-      {'from': 'b5', 'to': 'b5-2'},
-      {'from': 'b5', 'to': 'b5-3'},
-      {'from': 'b5', 'to': 'b5-4'},
       {'from': 'b', 'to': 'b6'},
       {'from': 'b6', 'to': 'b6-1'},
-      {'from': 'b6', 'to': 'b6-2'},
-      {'from': 'b6', 'to': 'b6-3'},
-      {'from': 'b6', 'to': 'b6-4'},
-      {'from': 'b6', 'to': 'b6-5'},
-      {'from': 'c', 'to': 'c1'},
-      {'from': 'c1', 'to': 'c1-1'},
-      {'from': 'c1', 'to': 'c1-2'},
-      {'from': 'c1', 'to': 'c1-3'},
-      {'from': 'c1', 'to': 'c1-4'},
-      {'from': 'c1', 'to': 'c1-5'},
-      {'from': 'c1', 'to': 'c1-6'},
-      {'from': 'c1', 'to': 'c1-7'},
-      {'from': 'c', 'to': 'c2'},
-      {'from': 'c2', 'to': 'c2-1'},
-      {'from': 'c2', 'to': 'c2-2'},
-      {'from': 'c', 'to': 'c3'},
-      {'from': 'c3', 'to': 'c3-1'},
-      {'from': 'c3', 'to': 'c3-2'},
-      {'from': 'c3', 'to': 'c3-3'},
-      {'from': 'd', 'to': 'd1'},
-      {'from': 'd1', 'to': 'd1-1'},
-      {'from': 'd1', 'to': 'd1-2'},
-      {'from': 'd1', 'to': 'd1-3'},
-      {'from': 'd1', 'to': 'd1-4'},
-      {'from': 'd1', 'to': 'd1-5'},
-      {'from': 'd1', 'to': 'd1-6'},
-      {'from': 'd1', 'to': 'd1-7'},
-      {'from': 'd1', 'to': 'd1-8'},
-      {'from': 'd', 'to': 'd2'},
-      {'from': 'd2', 'to': 'd2-1'},
-      {'from': 'd2', 'to': 'd2-2'},
-      {'from': 'd', 'to': 'd3'},
-      {'from': 'd3', 'to': 'd3-1'},
-      {'from': 'd3', 'to': 'd3-2'},
-      {'from': 'd3', 'to': 'd3-3'},
-      {'from': 'd3', 'to': 'd3-4'},
-      {'from': 'd3', 'to': 'd3-5'},
-      {'from': 'd', 'to': 'd4'},
-      {'from': 'd4', 'to': 'd4-1'},
-      {'from': 'd4', 'to': 'd4-2'},
-      {'from': 'd4', 'to': 'd4-3'},
-      {'from': 'd4', 'to': 'd4-4'},
-      {'from': 'd4', 'to': 'd4-5'},
-      {'from': 'd4', 'to': 'd4-6'},
       {'from': 'a', 'to': 'e'},
       {'from': 'e', 'to': 'e1'},
       {'from': 'e1', 'to': 'e1-1'},
-      {'from': 'e1', 'to': 'e1-2'},
-      {'from': 'e1', 'to': 'e1-3'},
-      {'from': 'e1', 'to': 'e1-4'},
-      {'from': 'e1', 'to': 'e1-5'},
-      {'from': 'e1', 'to': 'e1-6'},
-      {'from': 'e', 'to': 'e2'},
-      {'from': 'e2', 'to': 'e2-1'},
-      {'from': 'e2', 'to': 'e2-2'},
-      {'from': 'e2', 'to': 'e2-3'},
-      {'from': 'e2', 'to': 'e2-4'},
-      {'from': 'e2', 'to': 'e2-5'},
-      {'from': 'e2', 'to': 'e2-6'},
-      {'from': 'e2', 'to': 'e2-7'},
-      {'from': 'e2', 'to': 'e2-8'},
-      {'from': 'e2', 'to': 'e2-9'}
     ]
   };
   graphRef.value.setJsonData(__graph_json_data, (graphInstance) => {
     // 这些写上当图谱初始化完成后需要执行的代码
   });
+
 };
 
 // 节点点击事件处理方法
 const onNodeClick = (nodeObject, $event) => {
   console.log('onNodeClick:', nodeObject);
-  const allChildIds = ['e', 'a', 'b', 'b4', 'b4-3', 'e1', 'e1-6'];
+  const allChildIds = ['e', 'a', 'b', 'b1',  'e1'];
   console.log(allChildIds, 'allChildIds');
   const graphInstance = graphRef.value.getInstance();
   if (graphInstance) {
     const nodes = graphInstance.getNodes();
+
     for (const node of nodes) {
       if (allChildIds.includes(node.id)) {
         node.opacity = 1;
-        node.color = 'rgb(116,2,173)';
+        node.color = 'rgb(30 115 237)';
       } else {
         node.opacity = 0.1;
         node.color = undefined;
@@ -295,7 +194,7 @@ const onNodeClick = (nodeObject, $event) => {
     for (const link of links) {
       if (allChildIds.includes(link.fromNode.id) && allChildIds.includes(link.toNode.id)) {
         link.relations.forEach(line => {
-          line.color = 'rgb(116,2,173)';
+          line.color = 'rgb(30 115 237)';
         });
       } else {
         link.relations.forEach(line => {
@@ -305,6 +204,38 @@ const onNodeClick = (nodeObject, $event) => {
     }
   }
 };
+//查询某个节点
+const handleQuery = async () => {
+  console.log(queryParams);
+  const allChildIds = ['e', 'a', 'b', 'b1',  'e1'];
+  const graphInstance = graphRef.value.getInstance();
+  if (graphInstance) {
+    const searchResults = [];
+    const nodes = graphInstance.getNodes();
+    for (const node of nodes) {
+      if (allChildIds.includes(node.id)) {
+        node.opacity = 1;
+        node.color = 'rgb(30 115 237)';
+      } else {
+        node.opacity = 0.1;
+        node.color = undefined;
+      }
+    }
+    const links = graphInstance.getLinks();
+    for (const link of links) {
+      if (allChildIds.includes(link.fromNode.id) && allChildIds.includes(link.toNode.id)) {
+        link.relations.forEach(line => {
+          line.color = 'rgb(30 115 237)';
+        });
+      } else {
+        link.relations.forEach(line => {
+          line.color = '';
+        });
+      }
+    }
+  }
+};
+
 // 获取所有子节点ID的方法（原 deepGeAlltChildIds）
 const deepGeAlltChildIds = (node, ids = []) => {
   if (ids.includes(node.id)) return ids;
@@ -341,6 +272,29 @@ const onLineClick = (lineObject, linkObject, $event) => {
   console.log('onLineClick:', lineObject);
 };
 
+/** 查询所有模型列表 */
+function ModelSelectAll() {
+  dataModelSelectAll().then(response => {
+    optionsModel.value = response.data;
+
+  });
+}
+//选择模型
+const handleModelValueChange = (newValue) => {
+  if (newValue) {
+    queryParams.dataTableId=undefined
+    queryParams.dataColumnName=undefined
+    optionsTable.value=[]
+    ModelIdSelectAll(newValue)
+  }
+};
+/** 根据id查询数据表下拉列表 */
+function ModelIdSelectAll(ModelId) {
+  selectAllByModelId(ModelId).then(response => {
+    optionsTable.value = response.data;
+
+  });
+}
 </script>
 <style>
 .relation-graph .rel-map {
@@ -378,9 +332,13 @@ const onLineClick = (lineObject, linkObject, $event) => {
   background: linear-gradient(to right, rgb(16, 185, 129), rgb(101, 163, 13));
   //background: linear-gradient(to right, #add8e6, #ffffff);
   //background: linear-gradient(to right, #f8bbd0, #e1bee7);
-  //background: linear-gradient(to right, #2c3e50, #34495e);
+  background: linear-gradient(to right, #2c3e50, #34495e);
   //background: linear-gradient(to right, #ff9a8b, #ff6a88);
   //background: linear-gradient(to right, #00bcd4, #2196f3);
+  background: linear-gradient(to right, #000000, #1a1a1a);
+  background: linear-gradient(to right, #000000, #333333);
+  //background: linear-gradient(to right, #00001a, #17174b);
+  background: linear-gradient(to right, #3ca3e3, rgb(140, 95, 231));
 }
 
 ::v-deep .relation-graph {
